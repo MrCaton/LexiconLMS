34c2d6a6793e986ce0a12e18af881b375094392d
Iván Araque Cristóbal <ivanaraquecristobal@gmail.com>
2020-11-10 14:43:52 +0100

    Some controllers

---
+++ LexiconLMS/Data/LexiconLMSContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LexiconLMS.Models;
+
+namespace LexiconLMS.Data
+{
+    public class LexiconLMSContext : DbContext
+    {
+        public LexiconLMSContext (DbContextOptions<LexiconLMSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LexiconLMS.Models.Activities> Activities { get; set; }
+
+        public DbSet<LexiconLMS.Models.Course> Course { get; set; }
+
+        public DbSet<LexiconLMS.Models.Group> Group { get; set; }
+    }
+}
