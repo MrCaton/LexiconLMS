527b240e5a76cf8e543cb37b484257b3b5bb2f42
Iván Araque Cristóbal <ivanaraquecristobal@gmail.com>
2020-11-10 14:06:54 +0100

    Add project files.

---
+++ LexiconLMS/Models/ActivityTypeEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconLMS.Models
+{
+    public enum ActivityTypeEnum
+    {
+       
+        ELearning,
+        Lecture,
+        Excercise,
+        Assigment,
+        Other
+
+    }
+}
---
+++ LexiconLMS/Models/Group.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconLMS.Models
+{
+    public class Group
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+
+    }
+}
