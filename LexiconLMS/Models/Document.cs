1e234ad9a6182ee23e20df2c076affbe00f312a5
Iván Araque Cristóbal <ivanaraquecristobal@gmail.com>
2020-11-12 09:32:27 +0100

    Fix one error

--- LexiconLMS/Models/Document.cs
+++ LexiconLMS/Models/Document.cs
@@ -11,7 +11,7 @@ namespace LexiconLMS.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime dateCreated{ get; set;}
-        public int? CourseId { get; set; }
+        
 
         //Foreign Key
         public int CourseId { get; set; }
